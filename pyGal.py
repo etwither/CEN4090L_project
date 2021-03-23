1aba4cfee80c3899f2f5c5b641126d461ddb0f20
etwither <etwitherspoon76@gmail.com>
2021-03-23 18:23:12 -0400

    player is able to shoot laser now

--- pyGal.py
+++ pyGal.py
@@ -29,19 +29,41 @@ class player(object):
         pygame.draw.rect(win, (0, 0, 175), (self.x-(self.width-22.5),self.y+(self.height-20),(self.width-15)*3,5))
         pygame.draw.rect(win, (0, 0, 175), (self.x-22.5,self.y+5,self.width-25,25))
         pygame.draw.rect(win, (0, 0, 175), (self.x+(self.width+12.5),self.y+5,self.width-25,25))
+        
+class laser(object):
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.width = 5
+        self.height = 10
+        self.vel = 15
+    
+    def draw(self,win):
+        pygame.draw.rect(win, (255, 0, 0), (self.x,self.y,self.width,self.height))
+
     
 #updates the player and the enemies        
 def redraw():
     win.fill((0,0,0))
     player1.draw(win)
+    for x in pLaser:
+        x.draw(win)
     pygame.display.update()
+
+
 
 run = True
 intro = True
 clock = pygame.time.Clock()
 
+#laser mechanics
+pLaser = []
+shoot = 0
+
 #player/enemies
 player1 = player(250,600,35,35)
+
+
 
 
 #main loop for game
@@ -53,6 +75,18 @@ while run:
         if event.type == pygame.QUIT:
             run = False
             break
+            
+    if shoot > 0:
+        shoot += 1
+    if shoot > 7:
+        shoot = 0        
+    
+    #move the player laser and if it leaves the screen delete it
+    for lazer in pLaser:
+        if lazer.y < winH and lazer.y > 0:
+            lazer.y -= lazer.vel
+        else:
+            pLaser.pop(pLaser.index(lazer))
             
     #gets the key pressed for movment
     keys = pygame.key.get_pressed()
@@ -86,6 +120,12 @@ while run:
     #move up
     elif keys[pygame.K_UP] and player1.y > 600:
         player1.y -= player1.vel
+        
+    #shoot laser
+    elif keys[pygame.K_SPACE] and shoot == 0:
+        if len(pLaser) < 2:
+            pLaser.append(laser(player1.x+17.5,player1.y-10)) 
+        shoot = 1
     
     redraw()
     
