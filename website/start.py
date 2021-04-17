be5e1c516cdd6d5e5f3f16a424217655a3174669
etwither <etwitherspoon76@gmail.com>
2021-04-17 15:20:55 -0400

    route to start Galaga and go to info page added

--- website/start.py
+++ website/start.py
@@ -2,11 +2,21 @@ from flask import Flask, render_template, request
 import sqlite3 as sql
 import datetime
 
+from subprocess import call
 app = Flask(__name__,template_folder='./')  
 
 @app.route('/')
 def home():
     return render_template('home.html') 
+    
+@app.route('/games')
+def games():
+    return render_template('pyGames.html')
+    
+@app.route('/pyGalaga')
+def pyGalaga():
+    call(["python3", "pyGal.py"])
+    return render_template('pyGal.html') 
     
 if __name__ == '__main__':
     app.run(debug = True)
