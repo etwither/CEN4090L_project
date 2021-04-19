fbefd16c9a05b10bc28e1bc7c69df8ec882bfe84
etwither <etwitherspoon76@gmail.com>
2021-04-19 18:10:22 -0400

    open and close phrases are now added

--- website/startDatabase.py
+++ website/startDatabase.py
@@ -1,8 +1,9 @@
 import sqlite3
 
 conn = sqlite3.connect('storeData.db')
+print ("Opened database successfully")
 
 conn.execute('CREATE TABLE Reviews (Username VARCHAR(40), Game VARCHAR(50), ReviewTime DATETIME, Rating FLOAT, Review VARCHAR(500))')
 
+print ("Table created successfully")
 conn.close()
-
