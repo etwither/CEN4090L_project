9007ad147dacf0b0bb663a4cfca24029969e343c
etwither <etwitherspoon76@gmail.com>
2021-04-20 20:38:29 -0400

    comments added

--- website/startDatabase.py
+++ website/startDatabase.py
@@ -1,9 +1,9 @@
 import sqlite3
 
-conn = sqlite3.connect('storeData.db')
+conn = sqlite3.connect('storeData.db')      #opens the database
 print ("Opened database successfully")
 
-conn.execute('CREATE TABLE Reviews (Username VARCHAR(40), Game VARCHAR(50), ReviewTime DATETIME, Rating FLOAT, Review VARCHAR(500))')
+conn.execute('CREATE TABLE Reviews (Username VARCHAR(40), Game VARCHAR(50), ReviewTime DATETIME, Rating FLOAT, Review VARCHAR(500))')  #creates the table
 
 print ("Table created successfully")
 conn.close()
