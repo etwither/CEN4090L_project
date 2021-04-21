9916d10c7ba9dbf5aa5dee3a9b99835d120abe51
etwither <etwitherspoon76@gmail.com>
2021-04-20 20:37:59 -0400

    comments added

--- website/start.py
+++ website/start.py
@@ -1,44 +1,51 @@
 from flask import Flask, render_template, request
 import sqlite3 as sql
 import datetime
-
 from subprocess import call
-app = Flask(__name__,template_folder='./')  
+app = Flask(__name__,template_folder='./')  #templates in the same folder as start.py
 
 #call(["python3", "startDatabase.py"])
 
+#opens home page
 @app.route('/')
 def home():
     return render_template('home.html') 
-    
+
+#opens game selection page
 @app.route('/games')
 def games():
     return render_template('pyGames.html')
-    
+
+#starts game and opens game info page
 @app.route('/EscapeArtist')
 def escape():
     call(["python3", "eaplay.py"])
     return render_template('escape.html')
     
+#starts game and opens game info page
 @app.route('/Galaga')
 def pyGalaga():
     call(["python3", "pyGal.py"])
     return render_template('pyGal.html')
     
+#starts game and opens game info page
 @app.route('/Pacman')
 def pacman():
     call(["python3", "pacman_main.py"])
     return render_template('pacman.html') 
     
+#starts game and opens game info page
 @app.route('/Pokemon')
 def pokemon():
     call(["python3", "main.py"])
     return render_template('pokemon.html') 
     
+#opens page to add reviews
 @app.route('/addReview')
 def new_review():
     return render_template('reviewAdder.html')
-
+    
+#adds new reviews into the Reviews table
 @app.route('/',methods = ['GET', 'POST'])
 def addr():
     if request.method == 'POST':
@@ -60,10 +67,12 @@ def addr():
             return render_template('home.html')  
             con.close()
 
+#opens page to search for reviews
 @app.route('/reviewSearch')                                       
 def search_review():
     return render_template('reviewSearch.html')
     
+#gets reviews for the selected game
 @app.route('/reviewsFound',methods = ['GET', 'POST'])    
 def show():
     rows = []
@@ -82,11 +91,13 @@ def show():
         finally:
             return render_template("reviewsDisplay.html",rows = rows,game = game)  
             con.close()
-            
+     
+#opens login page       
 @app.route('/Login')
 def login():
     return render_template('login.html')
 
+#checks to see if the name and password are correct
 @app.route('/removeReviews',methods = ['GET', 'POST'])
 def log():
     if request.method == 'POST':
@@ -107,11 +118,13 @@ def log():
                     return render_template('error.html')
             else:
                 return render_template('error.html')
-                
+
+#opens the page to remove a review
 @app.route('/remove')
 def rm():
     return render_template('remove.html')
                 
+#removes a review with the matching username and game
 @app.route('/remove2',methods = ['GET', 'POST'])
 def remove():
     if request.method == 'POST':
